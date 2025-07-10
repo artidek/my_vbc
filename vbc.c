d7143a3012d1595a04984762126215b4cae784bf
aobshatk <obshatko.artem@gmail.com>
2025-07-10 23:03:43 +0200

    need to implement brace logic

--- vbc.c
+++ vbc.c
@@ -1,160 +1,231 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct s_b_tree
 {
-	enum {ADD, MULT, VAL} type;
-	int val;
-	struct s_b_tree *l;
-	struct s_b_tree *r;
-} b_tree;
+	enum
+	{
+		ADD,
+		MULT,
+		VAL
+	} type;
+	int				val;
+	struct s_b_tree	*l;
+	struct s_b_tree	*r;
+}					b_tree;
 
-b_tree *new_node(b_tree node)
+b_tree	*new_node(b_tree node)
 {
-	b_tree *new_node = calloc(sizeof(node), 1);
+	b_tree	*new_node;
+
+	new_node = calloc(sizeof(node), 1);
 	if (!new_node)
 		return (NULL);
 	*new_node = node;
 	return (new_node);
 }
 
-void pop_tree(b_tree **tree, b_tree node)
+int	add_leaf(b_tree *branch, b_tree leaf)
 {
-	if (!*tree)
-	{
-		*tree = new_node(node);
-		return;
-	}
-	b_tree *temp = *tree;
-	if (temp->type == VAL || temp->type == MULT)
-	{
-		*tree = new_node(node);
-		(*tree)->l = temp;
-	}
-	else
-	{
-		*tree = new_node(node);
-		(*tree)->r = temp;
-	}
-}
+	b_tree	*temp;
 
-int traverse_tree (b_tree *tree)
-{
-	if (tree)
+	if (!branch->l)
 	{
-		switch (tree->type)
-		{
-			case ADD:
-				return (traverse_tree(tree->l) + traverse_tree(tree->r));
-				break;
-			case MULT:
-				return (traverse_tree(tree->l) * traverse_tree(tree->r));
-				break;
-			case VAL:
-				return (tree->val);
-				break;
-			default:
-				break;
-		}
+		branch = new_node(leaf);
+		return (1);
+	}
+	else if (branch->l && leaf.type != VAL)
+	{
+		temp = branch->l;
+		branch->l = new_node(leaf);
+		branch->l->l = temp;
+		return (1);
+	}
+	if (!branch->r)
+	{
+		branch->r = new_node(leaf);
+		return (1);
+	}
+	else if (branch->r && leaf.type != VAL)
+	{
+		temp = branch->r;
+		branch->r = new_node(leaf);
+		branch->r->r = temp;
+		return (1);
 	}
 	return (0);
 }
 
-void push_tree(b_tree **tree, b_tree node)
+void	pop_tree(b_tree **tree, b_tree node)
 {
+	b_tree	*temp;
+
 	if (!*tree)
 	{
 		*tree = new_node(node);
-		return;
+		return ;
 	}
-	b_tree *temp = *tree;
-	if (node.type == VAL)
+	temp = *tree;
+	*tree = new_node(node);
+	(*tree)->r = temp;
+}
+
+int	traverse_tree(b_tree *tree)
+{
+	switch (tree->type)
 	{
-		if (temp->l && temp->l->type != VAL)
-		{
-			while(temp->l && temp->l->type != VAL)
-				temp = temp->l;
-			if (!temp->l)
-			{
-				temp->l = new_node(node);
-			}
-			else
-				temp->r = new_node(node);
-			return;
-		}
-		if (temp->r && temp->r->type != VAL)
-		{
-			while(temp->r && temp->r->type != VAL)
-				temp = temp->r;
-			if (!temp->r)
-			{
-				temp->r = new_node(node);
-			}
-			else
-				temp->l = new_node(node);
-		}
+	case ADD:
+		return (traverse_tree(tree->l) + traverse_tree(tree->r));
+		break ;
+	case MULT:
+		return (traverse_tree(tree->l) * traverse_tree(tree->r));
+		break ;
+	case VAL:
+		return (tree->val);
+		break ;
+	default:
+		break ;
 	}
-	else
+}
+
+void	push_tree(b_tree **tree, b_tree node)
+{
+	b_tree	*temp;
+
+	if (!*tree)
 	{
-		if (temp->r)
+		*tree = new_node(node);
+		return ;
+	}
+	temp = *tree;
+	if (temp->type == VAL)
+	{
+		*tree = new_node(node);
+		(*tree)->r = temp;
+		return ;
+	}
+	if (!temp->l)
+	{
+		temp->l = new_node(node);
+		return ;
+	}
+	if (!temp->r)
+	{
+		temp->r = new_node(node);
+		return ;
+	}
+	while (temp->l)
+	{
+		if (add_leaf(temp, node))
+			return ;
+		temp = temp->l;
+	}
+	temp = *tree;
+	while (temp->r)
+	{
+		if (add_leaf(temp, node))
+			return ;
+		temp = temp->r;
+	}
+}
+
+void after_brace(char *str, int *j, int *i, b_tree **tree)
+{
+	b_tree	node;
+	if (str[*j + 1])
+	{
+		if (str[*j + 1] == '+')
 		{
-			while (temp->r && temp->r->type != VAL)
-				temp = temp->r;
-			while (temp->l && temp->l->type != VAL)
-				temp = temp->l;
-			if (!temp->l)
-			{
-				temp->l = new_node(node);
-			}
-			else
-			{
-				b_tree *l_temp = temp->l;
-				temp->l = new_node(node);
-				temp->l->l = l_temp;
-			}
+			node = (b_tree){.type = ADD, .l = NULL, .r = NULL};
+			pop_tree(tree, node);
+			(*j)++;
+			(*i)+=2;
 		}
-		else if (temp->type == VAL)
+		else if (str[*j + 1] == '*')
 		{
-			*tree = new_node(node);
-			(*tree)->l = temp;
-		}
-		else if (!temp->l)
-		{
-			temp->l = new_node(node);
-		}
-		else if (temp->l)
-		{
-			while (temp->l && temp->l->type != VAL)
-				temp = temp->l;
-			if (!temp->l)
-			{
-				temp->l = new_node(node);
-			}
-			else
-			{
-				b_tree *l_temp = temp->l;
-				temp->l = new_node(node);
-				temp->l->l = l_temp;
-			}
+			node = (b_tree){.type = MULT, .l = NULL, .r = NULL};
+			pop_tree(tree, node);
+			(*j)++;
+			(*i)+=2;
 		}
 	}
 }
 
-int main (int argc, char **argv)
+void	parse_brace(char *str, int *i, b_tree **tree)
 {
+	int		j;
+	int		count_open;
+	int		count_close;
+	b_tree	node;
+
+	j = 0;
+	count_open = 0;
+	count_close = 0;
+	while (str[j])
+	{
+		if (str[j] == '(')
+			count_open++;
+		if (str[j] == ')')
+			break ;
+		j++;
+	}
+	j = 0;
+	while (count_close != count_open)
+	{
+		if (str[j] != '(' || str[j] != ')')
+		{
+			if (isdigit(str[j]))
+			{
+				node = (b_tree){.type = VAL, .val = str[j] - 48, .l = NULL,
+					.r = NULL};
+				push_tree(tree, node);
+			}
+			else if (str[j] == '+')
+			{
+				node = (b_tree){.type = ADD, .l = NULL, .r = NULL};
+				push_tree(tree, node);
+			}
+			else if (str[j] == '*')
+			{
+				node = (b_tree){.type = MULT, .l = NULL, .r = NULL};
+				push_tree(tree, node);
+			}
+		}
+		if (str[j] == ')')
+		{
+			after_brace(&str[j], &j, i, tree);
+			count_close++;
+		}
+		if (count_close != count_open)
+		{
+			j++;
+			(*i)++;
+		}
+		printf("str[j] %c\n", str[j]);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	*parse_str;
+	b_tree	node;
+	b_tree	*tree;
+	b_tree	*temp;
+
 	if (argc < 2)
 		return (1);
-	int i = 0;
-	char *parse_str = argv[1];
-	b_tree node;
-	b_tree *tree = NULL;
+	i = 0;
+	parse_str = argv[1];
+	tree = NULL;
 	while (parse_str[i])
 	{
 		if (isdigit(parse_str[i]))
 		{
-			node = (b_tree){.type = VAL, .val = parse_str[i] - 48, .l = NULL, .r = NULL};
+			node = (b_tree){.type = VAL, .val = parse_str[i] - 48, .l = NULL,
+				.r = NULL};
 			push_tree(&tree, node);
 		}
 		else if (parse_str[i] == '+')
@@ -167,16 +238,25 @@ int main (int argc, char **argv)
 			node = (b_tree){.type = MULT, .l = NULL, .r = NULL};
 			push_tree(&tree, node);
 		}
+		else if (parse_str[i] == '(')
+			parse_brace(&parse_str[i], &i, &tree);
 		i++;
 	}
-	// while (tree->r)
+	temp = tree;
+	// while (temp->l)
 	// {
-	// 	printf("   %d\n", tree->type);
-	// 	if (tree->l)
-	// 		printf ("left %d      ", tree->l->type);
-	// 	if (tree->r)
-	// 		printf("right %d\n", tree->r->type);
-	// 	tree = tree->r;
+	// 	printf("   %p\n", temp);
+	// 	printf("left %p      ", temp->l);
+	// 	printf("right %p\n", temp->r);
+	// 	temp = temp->l;
+	// }
+	// temp = tree;
+	// while (temp->r)
+	// {
+	// 	printf("   %p\n", temp);
+	// 	printf("left %p      ", temp->l);
+	// 	printf("right %p\n", temp->r);
+	// 	temp = temp->r;
 	// }
 	printf("result: %d\n", traverse_tree(tree));
 	return (0);
